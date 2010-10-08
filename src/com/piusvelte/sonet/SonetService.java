154f55cf579ff46580f060d87a44164ae6f1ff32
Bryan Emmanuel <piusvelte@gmail.com>
2010-10-08 02:28:30 +0000

    android-sonet update

--- src/com/piusvelte/sonet/SonetService.java
+++ src/com/piusvelte/sonet/SonetService.java
@@ -29,7 +29,6 @@ import static com.piusvelte.sonet.Sonet.TAG;
 import static com.piusvelte.sonet.Sonet.TWITTER_KEY;
 import static com.piusvelte.sonet.Sonet.TWITTER_SECRET;
 
-import java.util.HashMap;
 import java.util.List;
 
 import twitter4j.Status;
@@ -46,9 +45,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.os.IBinder;
 import android.util.Log;
@@ -74,128 +73,84 @@ public class SonetService extends Service {
 
 	public void init() {
 		((AlarmManager) getSystemService(Context.ALARM_SERVICE)).cancel(PendingIntent.getBroadcast(this, 0, new Intent(this, SonetService.class).setAction(REFRESH), 0));
-		SharedPreferences sp = (SharedPreferences) getSharedPreferences(getString(R.string.key_preferences), SonetService.MODE_PRIVATE);
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-		boolean hasConnection = cm.getBackgroundDataSetting() && cm.getActiveNetworkInfo().isConnected();
-		SonetDatabaseHelper sonetDatabaseHelper = new SonetDatabaseHelper(this);
-		SQLiteDatabase db = sonetDatabaseHelper.getWritableDatabase();
 		// Perform this loop procedure for each App Widget that belongs to this provider
 		AppWidgetManager manager = AppWidgetManager.getInstance(this);
-		// Build an update that holds the updated widget contents
-		RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget);
-		// set buttons
-		views.setOnClickPendingIntent(R.id.button_post, PendingIntent.getBroadcast(this, 0, (new Intent(this, UI.class)), 0));
-		views.setOnClickPendingIntent(R.id.button_configure, PendingIntent.getBroadcast(this, 0, (new Intent(this, UI.class)), 0));
-		views.setOnClickPendingIntent(R.id.button_refresh, PendingIntent.getBroadcast(this, 0, (new Intent(this, SonetService.class)).setAction(REFRESH), 0));
-		// set head styles
-		int status_count = 0;
-		HashMap<String, Integer> friend_map = new HashMap<String, Integer>();
-		String r_friend = "R.id.friend_";
-		friend_map.put(r_friend+"0", new Integer(R.id.friend_0));
-		friend_map.put(r_friend+"1", new Integer(R.id.friend_1));
-		friend_map.put(r_friend+"2", new Integer(R.id.friend_2));
-		friend_map.put(r_friend+"3", new Integer(R.id.friend_3));
-		friend_map.put(r_friend+"4", new Integer(R.id.friend_4));
-		friend_map.put(r_friend+"5", new Integer(R.id.friend_5));
-		friend_map.put(r_friend+"6", new Integer(R.id.friend_6));
-		friend_map.put(r_friend+"7", new Integer(R.id.friend_7));
-		friend_map.put(r_friend+"8", new Integer(R.id.friend_8));
-		friend_map.put(r_friend+"9", new Integer(R.id.friend_9));
-		friend_map.put(r_friend+"10", new Integer(R.id.friend_10));
-		friend_map.put(r_friend+"11", new Integer(R.id.friend_11));
-		friend_map.put(r_friend+"12", new Integer(R.id.friend_12));
-		friend_map.put(r_friend+"13", new Integer(R.id.friend_13));
-		friend_map.put(r_friend+"14", new Integer(R.id.friend_14));
-		friend_map.put(r_friend+"15", new Integer(R.id.friend_15));
-		friend_map.put(r_friend+"16", new Integer(R.id.friend_16));
-		friend_map.put(r_friend+"17", new Integer(R.id.friend_17));
-		friend_map.put(r_friend+"18", new Integer(R.id.friend_18));
-		HashMap<String, Integer> status_map = new HashMap<String, Integer>();
-		String r_status = "R.id.status_";
-		friend_map.put(r_status+"0", new Integer(R.id.status_0));
-		friend_map.put(r_status+"1", new Integer(R.id.status_1));
-		friend_map.put(r_status+"2", new Integer(R.id.status_2));
-		friend_map.put(r_status+"3", new Integer(R.id.status_3));
-		friend_map.put(r_status+"4", new Integer(R.id.status_4));
-		friend_map.put(r_status+"5", new Integer(R.id.status_5));
-		friend_map.put(r_status+"6", new Integer(R.id.status_6));
-		friend_map.put(r_status+"7", new Integer(R.id.status_7));
-		friend_map.put(r_status+"8", new Integer(R.id.status_8));
-		friend_map.put(r_status+"9", new Integer(R.id.status_9));
-		friend_map.put(r_status+"10", new Integer(R.id.status_10));
-		friend_map.put(r_status+"11", new Integer(R.id.status_11));
-		friend_map.put(r_status+"12", new Integer(R.id.status_12));
-		friend_map.put(r_status+"13", new Integer(R.id.status_13));
-		friend_map.put(r_status+"14", new Integer(R.id.status_14));
-		friend_map.put(r_status+"15", new Integer(R.id.status_15));
-		friend_map.put(r_status+"16", new Integer(R.id.status_16));
-		friend_map.put(r_status+"17", new Integer(R.id.status_17));
-		friend_map.put(r_status+"18", new Integer(R.id.status_18));
-		if (hasConnection) {
-			// Build the widget update for today
-			// sort results
-			// update
-			// Pick out month names from resources
-			//			Resources res = context.getResources();
-
-			// query accounts
-			Cursor cursor = db.query(TABLE_ACCOUNTS, new String[]{_ID, USERNAME, TOKEN, SECRET, SERVICE}, null, null, null, null, null);
-			if (cursor.getCount() > 0) {
-				cursor.moveToFirst();
-				int service = cursor.getColumnIndex(SERVICE),
-				token = cursor.getColumnIndex(TOKEN),
-				secret = cursor.getColumnIndex(SECRET);
-				while (!cursor.isAfterLast()) {
-					switch (cursor.getInt(service)) {
-					case TWITTER:
-						TwitterFactory factory = new TwitterFactory();
-						Twitter twitter = factory.getInstance();
-						twitter.setOAuthConsumer(TWITTER_KEY, TWITTER_SECRET);
-						AccessToken accessToken = new AccessToken(cursor.getString(token), cursor.getString(secret));
-						twitter.setOAuthAccessToken(accessToken);
-						try {
-							List<Status> statuses = twitter.getFriendsTimeline();
-							for (Status status : statuses) {
-								if (status_count < 19) {
-									views.setTextViewText(friend_map.get(r_friend+Integer.toString(status_count)).intValue(), status.getUser().getScreenName());
-									views.setTextViewText(status_map.get(r_status+Integer.toString(status_count)).intValue(), status.getText());
-								}
-								status_count++;
-							}
-						} catch (TwitterException te) {
-							Log.e(TAG, te.toString());
-						}
-						break;
-					case FACEBOOK:
-						break;					
-					}
-					cursor.moveToNext();
-				}
-			}
-			cursor.close();
-			// list content
-			// empty list will display "loading..."
-			// apply styles
-		} else {
-			views.setTextViewText(R.id.friend_0, "");
-			views.setTextViewText(R.id.status_0, getString(R.string.no_connection));
-			status_count++;
-		}
-		db.close();
-		// reset unused statuses
-		while (status_count < 19) {
-			views.setTextViewText(friend_map.get(r_friend+Integer.toString(status_count)).intValue(), "");
-			views.setTextViewText(status_map.get(r_status+Integer.toString(status_count)).intValue(), "");
-			status_count++;
-		}
-		// Push update for this widget to the home screen
 		int[] sonetWidget_4x2 = manager.getAppWidgetIds(new ComponentName(this, SonetWidget_4x2.class));
-		for (int i=0; i<sonetWidget_4x2.length; i++) manager.updateAppWidget(sonetWidget_4x2[i], views);
 		int[] sonetWidget_4x3 = manager.getAppWidgetIds(new ComponentName(this, SonetWidget_4x3.class));
-		for (int i=0; i<sonetWidget_4x3.length; i++) manager.updateAppWidget(sonetWidget_4x3[i], views);
 		int[] sonetWidget_4x4 = manager.getAppWidgetIds(new ComponentName(this, SonetWidget_4x4.class));
-		for (int i=0; i<sonetWidget_4x4.length; i++) manager.updateAppWidget(sonetWidget_4x4[i], views);
-		((AlarmManager) getSystemService(Context.ALARM_SERVICE)).set(AlarmManager.RTC, System.currentTimeMillis() + Integer.parseInt((String) sp.getString(getString(R.string.key_interval), getString(R.string.default_interval))), PendingIntent.getService(this, 0, new Intent(this, SonetService.class), 0));
+		if ((sonetWidget_4x2.length > 0) || (sonetWidget_4x3.length > 0) || (sonetWidget_4x4.length > 0)) {
+			SharedPreferences sp = (SharedPreferences) getSharedPreferences(getString(R.string.key_preferences), SonetService.MODE_PRIVATE);
+			ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+			boolean hasConnection = cm.getBackgroundDataSetting() && cm.getActiveNetworkInfo().isConnected();
+			SonetDatabaseHelper sonetDatabaseHelper = new SonetDatabaseHelper(this);
+			SQLiteDatabase db = sonetDatabaseHelper.getWritableDatabase();
+			// Build an update that holds the updated widget contents
+			RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget);
+			// set buttons
+			int head_text = Color.parseColor(sp.getString(getString(R.string.key_head_text), getString(R.string.default_head_text)));
+			views.setOnClickPendingIntent(R.id.button_post, PendingIntent.getBroadcast(this, 0, (new Intent(this, UI.class)), 0));
+			views.setTextColor(R.id.button_post, head_text);
+			views.setOnClickPendingIntent(R.id.button_configure, PendingIntent.getBroadcast(this, 0, (new Intent(this, UI.class)), 0));
+			views.setTextColor(R.id.button_post, head_text);
+			views.setOnClickPendingIntent(R.id.button_refresh, PendingIntent.getBroadcast(this, 0, (new Intent(this, SonetService.class)), 0));
+			views.setTextColor(R.id.button_post, head_text);
+			// set head styles
+			views.removeAllViews(R.id.body);
+			if (hasConnection) {
+				// Build the widget update for today
+				// sort results
+				// update
+				// Pick out month names from resources
+				//			Resources res = context.getResources();
+
+				// query accounts
+				Cursor cursor = db.query(TABLE_ACCOUNTS, new String[]{_ID, USERNAME, TOKEN, SECRET, SERVICE}, null, null, null, null, null);
+				if (cursor.getCount() > 0) {
+					cursor.moveToFirst();
+					int service = cursor.getColumnIndex(SERVICE),
+					token = cursor.getColumnIndex(TOKEN),
+					secret = cursor.getColumnIndex(SECRET),
+					body_text = Color.parseColor(sp.getString(getString(R.string.key_body_text), getString(R.string.default_body_text)));
+					while (!cursor.isAfterLast()) {
+						switch (cursor.getInt(service)) {
+						case TWITTER:
+							TwitterFactory factory = new TwitterFactory();
+							Twitter twitter = factory.getInstance();
+							twitter.setOAuthConsumer(TWITTER_KEY, TWITTER_SECRET);
+							AccessToken accessToken = new AccessToken(cursor.getString(token), cursor.getString(secret));
+							twitter.setOAuthAccessToken(accessToken);
+							try {
+								List<Status> statuses = twitter.getFriendsTimeline();
+								for (Status status : statuses) {
+									RemoteViews v = new RemoteViews(getPackageName(), R.layout.friend_status);
+									v.setTextViewText(R.id.friend, status.getUser().getScreenName());
+									v.setTextColor(R.id.friend, body_text);
+									v.setTextViewText(R.id.status, status.getText());
+									v.setTextColor(R.id.status, body_text);
+									views.addView(R.id.body, v);
+								}
+							} catch (TwitterException te) {
+								Log.e(TAG, te.toString());
+							}
+							break;
+						case FACEBOOK:
+							break;					
+						}
+						cursor.moveToNext();
+					}
+				}
+				cursor.close();
+				// list content
+				// empty list will display "loading..."
+				// apply styles
+			}
+			db.close();
+			// Push update for this widget to the home screen
+			for (int i=0; i<sonetWidget_4x2.length; i++) manager.updateAppWidget(sonetWidget_4x2[i], views);
+			for (int i=0; i<sonetWidget_4x3.length; i++) manager.updateAppWidget(sonetWidget_4x3[i], views);
+			for (int i=0; i<sonetWidget_4x4.length; i++) manager.updateAppWidget(sonetWidget_4x4[i], views);
+			((AlarmManager) getSystemService(Context.ALARM_SERVICE)).set(AlarmManager.RTC, System.currentTimeMillis() + Integer.parseInt((String) sp.getString(getString(R.string.key_interval), getString(R.string.default_interval))), PendingIntent.getService(this, 0, new Intent(this, SonetService.class), 0));
+		}
 		// no need to keep the service around
 		stopSelf();
 	}
