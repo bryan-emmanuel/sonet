91ea645d12af33971a808487b00856933898c379
Bryan Emmanuel <piusvelte@gmail.com>
2010-10-29 02:20:43 +0000

    android-sonet update

--- src/com/piusvelte/sonet/Sonet.java
+++ src/com/piusvelte/sonet/Sonet.java
@@ -7,9 +7,9 @@ public class Sonet {
 	public static final String TWITTER_URL_REQUEST = "https://api.twitter.com/oauth/request_token";
 	public static final String TWITTER_URL_ACCESS = "https://api.twitter.com/oauth/access_token";
 	public static final String TWITTER_URL_AUTHORIZE = "https://api.twitter.com/oauth/authorize";
-	static final String TWITTER_KEY = "";
-	static final String TWITTER_SECRET = "";
-	static final String FACEBOOK_ID = "";
+	protected static final String TWITTER_KEY = "CJ0g7D23BouS76C8Eg79g";
+	protected static final String TWITTER_SECRET = "ueu63CreQOWoU3NTGrQkpCmbAnwH3Rtldm04PUNmo";
+	protected static final String FACEBOOK_ID = "156294134390647";
     public static final String[] FACEBOOK_PERMISSIONS = new String[] {"offline_access"};
 	
 	public Sonet() {}
